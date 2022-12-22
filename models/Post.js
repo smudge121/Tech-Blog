8c5c6015b41efdbf90c80107ab9fe26257a4dac0
Jishii <33364545+smudge121@users.noreply.github.com>
2022-12-21 20:33:58 -0800

    Added auto generated date to posts

--- models/Post.js
+++ models/Post.js
@@ -19,6 +19,10 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
+        date: {
+            type: DataTypes.DATEONLY,
+            defaultValue: DataTypes.NOW
+        },
         user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
