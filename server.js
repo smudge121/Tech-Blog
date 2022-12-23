7b99f0082c375d1e980075362bde1cf04be20431
Jishii <33364545+smudge121@users.noreply.github.com>
2022-12-22 17:26:20 -0800

    Connects to blank home page

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
