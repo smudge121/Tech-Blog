e18d2a8eac8d23502e72836941d76b6bf500c289
Jishii <33364545+smudge121@users.noreply.github.com>
2022-12-22 17:41:15 -0800

    Home page now displays posts

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homepage')
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true}));
+
+        res.render('homepage', {
+            posts
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
+
+router.get('/profile', async (req, res) => {
+    res.render('profile');
+})
 
 module.exports = router;
